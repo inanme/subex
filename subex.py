efa2ce098d42eef5727efafd2480f4d1bc850a0f
mert inan <inanme@gmail.com>
2017-10-29 18:19:11 +0000

    gitignore vs for comprehensions

--- subex.py
+++ subex.py
@@ -18,25 +18,41 @@ def decorate_string(string):
     return "{} - {} \n".format(now, string)
 
 
+def column2(window):
+    window.run_command('set_layout', {
+        'cols': [0.0, 0.5, 1.0],
+        'rows': [0.0, 1.0],
+        'cells': [[0, 0, 1, 1], [1, 0, 2, 1]]
+    })
+
+
+def new_file(window, name):
+    if window.num_groups() < 2:
+        column2(window)
+    output_view = window.new_file()
+    output_view.set_name(name)
+    window.set_view_index(output_view, 1, 0)
+    return output_view
+
+
 def get_active_view_and_window():
     window = sublime.active_window()
     view = window.active_view()
     return view, window
 
 
-def get_active_view_line():
-    view, window = get_active_view_and_window()
-    for region in view.sel():
-        if region.empty():
-            line = view.line(region)
-            line_contents = view.substr(line)
-            return line_contents
+def get_active_view_line(view, _):
+    regions = [region for region in view.sel() if region.empty()]
+    region = regions[0] if regions else None
+    if region is not None:
+        line = view.line(region)
+        line_contents = view.substr(line)
+        return line_contents
 
 
-def echo_current_line_in_panel():
-    current_line = get_active_view_line()
+def echo_current_line_in_panel(view, window):
+    current_line = get_active_view_line(view, window)
     if current_line is not None:
-        view, window = get_active_view_and_window()
         existing_panel = window.find_output_panel(EchoCommand.cmd_name)
         panel = existing_panel if existing_panel is not None else window.create_output_panel(EchoCommand.cmd_name)
         window.run_command('show_panel', {'panel': EchoCommand.pnl_name})
@@ -47,33 +63,15 @@ def echo_current_line_in_panel():
                                      'scroll_to_end': True})
 
 
-def column2(window):
-    window.run_command('set_layout', {
-        'cols': [0.0, 0.5, 1.0],
-        'rows': [0.0, 1.0],
-        'cells': [[0, 0, 1, 1], [1, 0, 2, 1]]
-    })
-
-
-def new_file(window):
-    if window.num_groups() < 2:
-        column2(window)
-    output_view = window.new_file()
-    output_view.set_name(EchoCommand.vw_name)
-    window.set_view_index(output_view, 1, 0)
-    return output_view
-
-
-def echo_current_line_in_view():
-    cr_view, window = get_active_view_and_window()
-    current_line = get_active_view_line()
+def echo_current_line_in_view(view, window):
+    current_line = get_active_view_line(view, window)
     if current_line is not None:
         existing_view = [view for view in window.views() if view.name() == EchoCommand.vw_name]
-        output_view = existing_view[0] if existing_view else new_file(window)
+        output_view = existing_view[0] if existing_view else new_file(window, EchoCommand.vw_name)
         output_view.run_command('insert', {'characters': decorate_string(current_line),
                                            'force': False,
                                            'scroll_to_end': True})
-        window.focus_view(cr_view)
+        window.focus_view(view)
 
 
 class SubexTextCommand(sublime_plugin.TextCommand):
@@ -86,9 +84,10 @@ class EchoCommand(SubexTextCommand):
     vw_name = 'Subex Output'
 
     def run(self, edit, output=None):
+        view, window = get_active_view_and_window()
         if output == 'panel':
-            echo_current_line_in_panel()
+            echo_current_line_in_panel(view, window)
         elif output == 'file':
-            echo_current_line_in_view()
+            echo_current_line_in_view(view, window)
         else:
             pass
